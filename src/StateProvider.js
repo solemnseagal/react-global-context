1632f72b5fb40ba2639465cbd3b8334e7b4f8da6
David Echoda <Davidechoda123@gmail.com>
2022-03-20 23:09:58 +0100

    file structure modification

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -2,7 +2,6 @@ import { useContext, useReducer, createContext } from "react";
 
 export const StateContext = createContext();
 export const StateProvider = ({ reducer, initialState, children }) => {
-  // console.log(useReducer(reducer, initialState));
   return (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
       {children}
